2e95e068df3322d6c04584dc6ac29726cb374383
iffah <iffahbatrisyiia@gmail.com>
2025-06-25 18:30:42 +0800

    hello

---
+++ omc 18/Form8.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form8
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox6_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Panel1_Paint_1(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub PictureBox6_Click_2(sender As Object, e As EventArgs) Handles PictureBox6.Click
+
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+
+    Private Sub PictureBox7_Click_1(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+    End Sub
+End Class
---
+++ omc 18/Module1.vb
@@ -0,0 +1,7 @@
+﻿Module Module1
+    Sub Main()
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form8())
+    End Sub
+End Module
