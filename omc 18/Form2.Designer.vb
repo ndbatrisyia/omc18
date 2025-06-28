866feec0f8a1760b568666d6bccfc3323c964cf4
iffah <iffahbatrisyiia@gmail.com>
2025-06-28 18:54:42 +0800

    dah delete guna properties

--- omc 18/Form2.Designer.vb
+++ omc 18/Form2.Designer.vb
@@ -22,43 +22,9 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnUser = New Guna.UI2.WinForms.Guna2Button()
-        Me.btnAdmin = New Guna.UI2.WinForms.Guna2Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'btnUser
-        '
-        Me.btnUser.BorderRadius = 15
-        Me.btnUser.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnUser.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnUser.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnUser.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnUser.FillColor = System.Drawing.Color.Thistle
-        Me.btnUser.Font = New System.Drawing.Font("Segoe Fluent Icons", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnUser.ForeColor = System.Drawing.Color.DarkOliveGreen
-        Me.btnUser.Location = New System.Drawing.Point(49, 334)
-        Me.btnUser.Name = "btnUser"
-        Me.btnUser.Size = New System.Drawing.Size(288, 45)
-        Me.btnUser.TabIndex = 0
-        Me.btnUser.Text = "Sign Up as User"
-        '
-        'btnAdmin
-        '
-        Me.btnAdmin.BorderRadius = 15
-        Me.btnAdmin.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnAdmin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnAdmin.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnAdmin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnAdmin.FillColor = System.Drawing.Color.LavenderBlush
-        Me.btnAdmin.Font = New System.Drawing.Font("Segoe Fluent Icons", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAdmin.ForeColor = System.Drawing.Color.DarkOliveGreen
-        Me.btnAdmin.Location = New System.Drawing.Point(49, 430)
-        Me.btnAdmin.Name = "btnAdmin"
-        Me.btnAdmin.Size = New System.Drawing.Size(288, 45)
-        Me.btnAdmin.TabIndex = 1
-        Me.btnAdmin.Text = "Sign Up as Admin"
         '
         'Label1
         '
@@ -84,17 +50,12 @@ Partial Class Form2
         Me.ClientSize = New System.Drawing.Size(386, 809)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.btnAdmin)
-        Me.Controls.Add(Me.btnUser)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form2"
         Me.Text = "Form2"
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents btnUser As Guna.UI2.WinForms.Guna2Button
-    Friend WithEvents btnAdmin As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
 End Class
--- omc 18/Form3.Designer.vb
+++ omc 18/Form3.Designer.vb
@@ -22,61 +22,7 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.txtName = New Guna.UI2.WinForms.Guna2TextBox()
-        Me.Guna2TextBox1 = New Guna.UI2.WinForms.Guna2TextBox()
         Me.SuspendLayout()
-        '
-        'txtName
-        '
-        Me.txtName.BorderColor = System.Drawing.Color.White
-        Me.txtName.BorderRadius = 10
-        Me.txtName.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.txtName.DefaultText = ""
-        Me.txtName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
-        Me.txtName.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
-        Me.txtName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.txtName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.txtName.FillColor = System.Drawing.Color.LavenderBlush
-        Me.txtName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.txtName.Font = New System.Drawing.Font("Segoe UI", 9.0!)
-        Me.txtName.ForeColor = System.Drawing.Color.Gray
-        Me.txtName.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.txtName.IconLeft = Global.omc_18.My.Resources.Resources.profile_icon_removebg_preview__1_
-        Me.txtName.Location = New System.Drawing.Point(50, 382)
-        Me.txtName.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.txtName.Name = "txtName"
-        Me.txtName.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.txtName.PlaceholderText = "Sign Up"
-        Me.txtName.SelectedText = ""
-        Me.txtName.Size = New System.Drawing.Size(286, 45)
-        Me.txtName.TabIndex = 1
-        Me.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
-        '
-        'Guna2TextBox1
-        '
-        Me.Guna2TextBox1.BorderColor = System.Drawing.Color.White
-        Me.Guna2TextBox1.BorderRadius = 10
-        Me.Guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.Guna2TextBox1.DefaultText = ""
-        Me.Guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
-        Me.Guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
-        Me.Guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.Guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.Guna2TextBox1.FillColor = System.Drawing.Color.LavenderBlush
-        Me.Guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Guna2TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
-        Me.Guna2TextBox1.ForeColor = System.Drawing.Color.Gray
-        Me.Guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Guna2TextBox1.IconLeft = Global.omc_18.My.Resources.Resources.profile_icon_removebg_preview__1_
-        Me.Guna2TextBox1.Location = New System.Drawing.Point(50, 467)
-        Me.Guna2TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Guna2TextBox1.Name = "Guna2TextBox1"
-        Me.Guna2TextBox1.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Guna2TextBox1.PlaceholderText = "Sign Up"
-        Me.Guna2TextBox1.SelectedText = ""
-        Me.Guna2TextBox1.Size = New System.Drawing.Size(286, 45)
-        Me.Guna2TextBox1.TabIndex = 2
-        Me.Guna2TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Form3
         '
@@ -84,15 +30,10 @@ Partial Class Form3
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Window
         Me.ClientSize = New System.Drawing.Size(386, 809)
-        Me.Controls.Add(Me.Guna2TextBox1)
-        Me.Controls.Add(Me.txtName)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form3"
         Me.Text = "Form3"
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents txtName As Guna.UI2.WinForms.Guna2TextBox
-    Friend WithEvents Guna2TextBox1 As Guna.UI2.WinForms.Guna2TextBox
 End Class
