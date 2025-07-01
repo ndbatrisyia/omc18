83247c82789b14f055ee08b66f2f7f34a680a2e9
qikeyyz <bqistina506@gmail.com>
2025-07-01 23:46:14 +0800

    haha

--- omc 18/LogInForms/Form9.Designer.vb
+++ omc 18/LogInForms/Form9.Designer.vb
@@ -22,9 +22,29 @@ Partial Class Form9
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(526, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form9
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form9"
         Me.Text = "Form9"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
