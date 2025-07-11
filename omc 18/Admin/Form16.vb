daf6bd76be7aa3ddf0717ad6e63d04dfbe024cc9
QURRATU NUR FAYYADHAH AHMAD MAWARIDI <qurratu_24006080@utp.edu.my>
2025-07-11 20:51:11 +0800

    test lagi

--- omc 18/Admin/Form16.vb
+++ omc 18/Admin/Form16.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form16
-
+    Private Sub DashboardToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DashboardToolStripMenuItem.Click
+        Form17.Show()
+        Me.Hide()
+    End Sub
 End Class
