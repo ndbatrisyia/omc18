9158f454112dac2eaebfc7f044eaa313b3e6e8cb
qikeyyz <bqistina506@gmail.com>
2025-07-12 18:17:36 +0800

    bro wa ubah dikit

--- omc 18/Main Page/Form10.vb
+++ omc 18/Main Page/Form10.vb
@@ -3,11 +3,25 @@
 
     End Sub
 
-    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+    Private Sub Button16_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Form8.Show()
+        Me.Hide()
     End Sub
 End Class
