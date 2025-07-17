ace7e9d750edf256bfc5adb0b4934d749892023f
iffah <iffahbatrisyiia@gmail.com>
2025-07-18 01:47:34 +0800

    database for real geng

--- omc 18/Module1.vb
+++ omc 18/Module1.vb
@@ -1,7 +1,13 @@
-﻿Module Module1
+﻿Imports System.Data.OleDb
+
+Module Module1
+    Public con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\iffah\source\repos\omc-18\omc 18\bin\Debug\Atelier.accdb")
+
     Sub Main()
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New Form8())
+
     End Sub
+
 End Module
