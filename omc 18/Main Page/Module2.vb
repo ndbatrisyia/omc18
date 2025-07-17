0cf6266b4c52f2651222bf1d107654d98240c810
iffah <iffahbatrisyiia@gmail.com>
2025-07-18 01:43:08 +0800

    databasegeng

---
+++ omc 18/Main Page/Module2.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Data.OleDb
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Menu
+
+Module Module2
+    Public con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\iffah\source\repos\omc-18\omc 18\bin\Debug\Atelier.accdb")
+
+End Module
